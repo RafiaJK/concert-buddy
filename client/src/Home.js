691a0b1d4675dc6da04ee376a9680fc68dda0f70
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-11-17 15:52:49 -0500

    updated home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,16 +3,20 @@ function Home(){
 
 
 return (
-    <div>
+    <div>  <div className="home-rectangle"></div>
+    <div className="homepage">
         <h2>Thank you for being part of Concert Buddy's community!</h2>
+        <h4>Live music brings all of us together - stay up to date on all your favorite shows, meet fellow fans, and get yourself new concert buddies! </h4>
         <h4> </h4>
-        <h3>Upcoming Features:</h3>
+        <br></br>
+        <h3>Stay tuned, we have some exciting upcoming features ahead:</h3>
         <li>Follow your favorite artists to find out when they're playing by you.</li>
         <li>Search by venue, artist, or date range.</li>
         <li>Add your photo and video memories from shows.</li>
-        <li>Expand beyond New York.</li>
+        <li>Expand our shows beyond New York.</li>
     </div>
 
+    </div>
 );
 }
 export default Home;
